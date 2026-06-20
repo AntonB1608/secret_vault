9138490e464353eab5c2f0a602dac04b962893d7
Anton Boewer <antonboewer@icloud.com>
2026-06-20 17:26:58 +0200

    add mail verification

--- app.py
+++ app.py
@@ -5,51 +5,108 @@ import datetime as dt
 from dotenv import load_dotenv
 import os
 from flask_wtf.csrf import CSRFProtect
+import secrets 
+from flask_mail import Mail, Message
+
 
 load_dotenv()
 app = Flask(__name__)
 app.config["SECRET_KEY"] = os.getenv("SECRET_KEY")
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///users.db"
+app.config['MAIL_SERVER'] = 'smtp.gmail.com'
+app.config['MAIL_PORT'] = 587
+app.config['MAIL_USERNAME'] = os.getenv("GMAIL_USER")
+app.config['MAIL_PASSWORD'] = os.getenv("GMAIL_PASSWORD")
+app.config['MAIL_USE_TLS'] = True
+app.config['MAIL_USE_SSL'] = False
 csrf = CSRFProtect(app)
-
+mail = Mail(app)
 db = SQLAlchemy(app)
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(20), unique=True, nullable=False)
-    password_hash = db.Column(db.String(200), nullable=False)
+    password_hash = db.Column(db.String(200), nullable=True)
     trys = db.Column(db.Integer, default=0)
+    email = db.Column(db.String(40), unique = True, nullable = False )
+    email_verified = db.Column(db.Boolean, default=False)
+    token = db.Column(db.String(32))
     locked_until = db.Column(db.DateTime, nullable=True)
+
+
+
 
 @app.route("/register", methods=["POST", "GET"])
 def register():
-    sonderzeichen = "!@#$%^&*()_+-=[]{}|;:',.<>?/~`"
+    
     if request.method == "POST":
         username = request.form["username"]
-        password = request.form["password"]
-        password_again = request.form["repeat_password"]
+        email = request.form["email"]
         if len(username) > 20:
             return render_template("register.html", fehlermeldung="username too long")
         user_exists = User.query.filter_by(username=username).first()
         if user_exists:
-            
-            return render_template("register.html", fehlermeldung = "Username already taken", password=password)
-
-        if len(password) < 15:
-            return render_template("register.html", fehlermeldung = "password to short", username=username)
-        has_sonderzeichen = any(zeichen in password for zeichen in sonderzeichen)
-        if not has_sonderzeichen:
-            return render_template("register.html", fehlermeldung = "password doesn't contain sonderzeichen", username=username)
-        password_hash = bcrypt.hashpw(password.encode("utf-8"), bcrypt.gensalt())
-        
-        if password == password_again:
-            new_user = User(username=username, password_hash=password_hash)
-            db.session.add(new_user)           
-            db.session.commit()
-            return redirect("/vault")
+            return render_template("register.html", fehlermeldung = "Username already taken")
+        email_exists = User.query.filter_by(email=email).first()
+        if email_exists:
+            return render_template("register.html", fehlermeldung = "email already taken", username=username)
         else:
-            return render_template("register.html", fehlermeldung = "passwords dont match", username=username, password=password)
+            token = secrets.token_urlsafe(32)
+            new_user = User(username=username, email=email, token=token, email_verified=False)
+            db.session.add(new_user)
+            db.session.commit()
+            msg = Message(
+            subject="Verify your email",
+            sender=os.getenv("GMAIL_USER"),
+            recipients=[email]
+            )
+            msg.body = f"Click this link to verify your email: http://localhost:5555/verify/{token}"
+            mail.send(msg)
+            return render_template("check_email.html")        
     else:
-        return render_template("register.html")
+        return render_template("register.html") 
+@app.route('/verify/<token>')
+def verifiy_user(token):    
+    user = User.query.filter_by(token = token).first()
+    if not user:
+        return "Invalid token"
+    user.email_verified = True
+    user.token = None
+    session["username"] = user.username
+    db.session.commit()
+    return redirect("/setpassword")
+
+@app.route('/setpassword', methods = ["POST", "GET"] )
+def setpassword():
+    if request.method == "POST":
+        username = session["username"]
+        user = User.query.filter_by(username=username).first()  
+        if user.email_verified == True and user.token == None:
+            sonderzeichen = "!@#$%^&*()_+-=[]{}|;:',.<>?/~`"
+            password = request.form["password"]
+            if len(password) < 15:
+                return render_template("setpassword.html", fehlermeldung = "password to short", username=username)
+            has_sonderzeichen = any(zeichen in password for zeichen in sonderzeichen)
+            if not has_sonderzeichen:
+                return render_template("setpassword.html", fehlermeldung = "password doesn't contain sonderzeichen", username=username)
+            password_hash = bcrypt.hashpw(password.encode("utf-8"), bcrypt.gensalt())
+            password_again = request.form["password_again"]
+            if password == password_again:
+                user.password_hash = password_hash          
+                db.session.commit()
+                return redirect("/login")
+            else:
+                return render_template("setpassword.html", fehlermeldung = "passwords dont match", username=username, password=password)
+        else: 
+            return render_template("register.html", fehlermeldung = "Your email is not verified yet.")
+    else:
+        return render_template("setpassword.html")
+
+
+
+
+
+        
+
 @app.route("/login", methods=["POST", "GET"])
 def login():
     current_date_time = dt.datetime.today()
